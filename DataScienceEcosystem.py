11bd001bb81dee7220106923d0328a752539efdd
antonis-nt <155850569+antonis-nt@users.noreply.github.com>
2025-02-08 17:13:20 +0200

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Create a markdown cell with the title Data Science Tools and Ecosystem using H1 style heading.
+
+
+# # Data Science Tools and Ecosystem 
+
+# In[5]:
+
+
+# Create a markdown cell for an introduction
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# In[13]:
+
+
+# Insert a markdown cell to list Objectives
+
+
+# **Objectives:**
+# - List popular languages for Data Science
+# - List popular libraries for Data Science
+# - List popular for Data Science Tools
+# - Converting 200 minutes to hours
+
+# In[6]:
+
+
+# Create a markdown cell to list data science languages
+
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala 
+# 5. C/C++
+# 6. JacaScript
+# 7. Matlab
+
+# In[7]:
+
+
+# Create a markdown cell to list data science libraries
+
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Pandas 
+# 3. Matplotlib
+# 4. Seaborn 
+# 5. Plotly
+# 6. Scikit-learn
+
+# In[ ]:
+
+
+# Create a markdown cell with a table of Data Science tools
+
+
+# | Data Science Tools |
+# | ------------------ |
+# | Jupyter Notebook |
+# | RStudio|
+# | Apache Hadoop |
+
+# In[8]:
+
+
+# Create a markdown cell introducing arithmetic expression examples
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+
+# In[ ]:
+
+
+# Create a code cell to multiply and add numbers
+
+
+# In[10]:
+
+
+#  This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[ ]:
+
+
+# Create a code cell to convert minutes to hours
+
+
+# In[12]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# In[14]:
+
+
+# Create a markdown cell to indicate the Author's name
+
+
+# ## Author 
+# Ntounis Antonios
+
+# In[ ]:
+
+
+
+
